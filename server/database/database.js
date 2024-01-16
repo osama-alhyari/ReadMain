8eb8fc7f5af9c4de806d5f1be6c35cd9eb5a7ac8
osama-alhyari <149674705+osama-alhyari@users.noreply.github.com>
2024-01-16 20:47:34 +0300

    preparing for azure deployment

--- server/database/database.js
+++ server/database/database.js
@@ -1,23 +1,32 @@
 import Sequelize from "sequelize";
-import 'dotenv/config'
+import "dotenv/config";
 
 const db = {};
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
-  host: process.env.HOST,
-  dialect: process.env.DIALECT,
-  define: {
-    timestamps: false,
-    freezeTableName: true,
-  },
-  operatorsAliases: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    driver: "tedious",
+    define: {
+      timestamps: false,
+      freezeTableName: true,
+    },
+    dialectOptions: {
+      encrypt: true,
+    },
+    operatorsAliases: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 db.sequelize = sequelize;
 
